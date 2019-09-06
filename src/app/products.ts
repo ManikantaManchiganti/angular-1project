7316a72db41746378f723b09879ba11400b73a77
ManikantaManchiganti <mmanikanta0007@gmail.com>
2019-09-06 15:00:41 +0530

    3 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -2,17 +2,20 @@ export const products = [
   {
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
+    img:""
   },
   {
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+    img: "https://cdn-media.kimovil.com/default/0002/83/thumb_182855_default_big.png"
   },
   {
     name: 'RedMI',
     price: 4000,
-    description: 'This is a next gemeration mobile with lots of features'
+    description: 'This is a next gemeration mobile with lots of features',
+    img:""
   }
 ];
 
